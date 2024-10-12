bf028b63bfaa81a32bc29486ed8f6a2aca01f317
ValorLin <qqq123026689@126.com>
2024-10-12 20:39:48 +0800

    fix baseurl

--- apps/scoreboard/vite.config.ts
+++ apps/scoreboard/vite.config.ts
@@ -5,6 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
+  base: "scoreboard",
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/scoreboard',
   server: {
